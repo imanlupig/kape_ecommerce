7625eb0e53da580d80de6edf46f43a1e503f5c0f
Ian Manlupig <imanlupig@academic.rrc.ca>
2024-11-19 03:58:26 -0600

    Added image attachment to Products model

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -4,12 +4,57 @@ ActiveAdmin.register Product do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  permit_params :name, :description, :price, :category_id
+  permit_params :name, :description, :price, :category_id, :image
 
   filter :name
   filter :description
   filter :price
   filter :category
+
+  # Products page table, modified to show images
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :description
+    column :price
+    column :category
+
+    column :image do |product|
+      if product.image.attached?
+        image_tag url_for(product.image), size: "100x100", style: "object-fit: contain; width: 100px; height: 100px;"
+      else
+        "No Image"
+      end
+    end
+
+    actions
+  end
+
+  # Modified form to accept image files
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :description
+      f.input :price
+      f.input :category
+      f.input :image, as: :file
+    end
+    f.actions
+  end
+
+  # Modified to show image
+  show do
+    attributes_table do
+      row :name
+      row :description
+      row :price
+      row :category
+      row :image do |product|
+        image_tag url_for(product.image), size: "400x400", style: "object-fit: contain; width: 400px; height: 400px;" if product.image.attached?
+      end
+    end
+  end
   #
   # or
   #
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :category
   has_many :order_items
+  has_one_attached :image # adds image attachment support
 
 
   def self.ransackable_associations(auth_object = nil)
