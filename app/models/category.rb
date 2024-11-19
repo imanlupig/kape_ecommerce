490650edb9d06ce90e47a0d90a784582056b6832
Ian Manlupig <imanlupig@academic.rrc.ca>
2024-11-19 02:54:09 -0600

    updated Models to include ransackable for ActiveAdmin

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,11 @@
 class Category < ApplicationRecord
   belongs_to :category
+
+  def self.ransackable_associations(auth_object = nil)
+    [ "category" ]
+  end
+
+  def self.ransackable_attributes(auth_object = nil)
+    [ "created_at", "id", "name", "updated_at" ]
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,11 @@
 class Product < ApplicationRecord
   has_many :products
+
+  def self.ransackable_associations(auth_object = nil)
+    [ "products" ]
+  end
+
+  def self.ransackable_attributes(auth_object = nil)
+    [ "category_id", "created_at", "description", "id", "name", "price", "updated_at" ]
+  end
 end
